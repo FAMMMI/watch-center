044de8269d074bb94adbf159944b41a58986998b
FAMMMI <naimfami@gmail.com>
2022-04-05 01:51:30 +0600

    not found page edited

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -4,7 +4,7 @@ const NotFound = () => {
     return (
         <div>
             <div>
-                <img className='w-fill h-80 mx-auto' src="https://cdn1.vectorstock.com/i/1000x1000/77/15/404-error-page-not-found-plug-graphic-background-vector-21467715.jpg" alt="" />
+                <img className='w-100 h-100 mx-auto' src="https://cdn1.vectorstock.com/i/1000x1000/77/15/404-error-page-not-found-plug-graphic-background-vector-21467715.jpg" alt="" />
             </div>
 
         </div>
